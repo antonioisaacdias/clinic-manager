f3bb34d07582b8ea7fd3f4e249dc9181dd07fd49
Antonio Dias <antonioisaacvd@gmail.com>
2025-04-16 12:20:14 -0300

    feat: implementa model Pacient

--- apps/patient/models.py
+++ apps/patient/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+
+
+class ModelPacient(models.Model):
+    
+    class Gender(models.TextChoices):
+        MALE = 'M', 'Masculino'
+        FEMALE = 'F', 'Feminino'
+        OTHER = 'O', 'Outro'
+        NOT_INFORMED = 'N', 'Prefere não informar'
+    
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    name = models.CharField(max_length=100, blank=False, null=False)
+    birth_date = models.DateField()
+    gender = models.CharField(max_length=1, choices=Gender.choices, default=Gender.NOT_INFORMED)
+    plan_card = models.CharField(max_length=100, unique=True)
+    phone = models.CharField(max_length=11)
+    guardian_name = models.CharField(max_length=100)
