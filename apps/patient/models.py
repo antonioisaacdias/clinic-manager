b144c8adc1a9996b6c36a305a01fde3accea8711
Antonio Dias <antonioisaacvd@gmail.com>
2025-04-17 21:39:31 -0300

    feat: Implementa app professionals

--- apps/patient/models.py
+++ apps/patient/models.py
@@ -13,13 +13,15 @@ class ModelPacient(models.Model):
         ('O', 'Outro'),
     ]
     
-    id = models.UUIDField(primary_key=True, default=uuid4)
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=100, blank=False, null=False)
     birth_date = models.DateField()
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     plan_card = models.CharField(max_length=100, unique=True, null=True, blank=True)
     phone = models.CharField(max_length=11)
     guardian_name = models.CharField(max_length=100, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)  # criado automaticamente
+    updated_at = models.DateTimeField(auto_now=True) 
     
     def get_gender_display_label(self):
         return dict(self.GENDER_CHOICES).get(self.gender, 'Desconhecido')
