6fc6877cec45da205980d2fcad9706b8372f3166
antonioisaacdias <antonioisaacvd@gmail.com>
2025-07-23 21:23:04 -0300

    feat: Adiciona models ao app administration.

---
+++ backend/apps/administration/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+class Plan(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100, unique=True)
+    logo = models.ImageField(upload_to='plan_logos/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+    
