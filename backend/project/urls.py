f99e0da470292832c2fe2bd70c3f45ce54b49a26
antonioisaacdias <antonioisaacvd@gmail.com>
2025-07-25 13:04:38 -0300

    feat: Adicioa rotas de autenticação no backend.

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView, TokenBlacklistView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +9,10 @@ urlpatterns = [
     path('api/v1/', include('apps.professional.urls')),
     path('api/v1/', include('apps.patient.urls')),
     path('api/v1/administration/', include('apps.administration.urls')),
+
+    # JWT Authentication
+    path('api/v1/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/v1/auth/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
 ]
