f9e017f4d96b953c8588f6ccb350fd6ddc1d9b1c
antonioisaacdias <antonioisaacvd@gmail.com>
2025-07-27 12:41:08 -0300

    feat: Implementa paginação e filtro na rota de especialidades

--- backend/apps/professional/views.py
+++ backend/apps/professional/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import action
 from .models import Professional, Specialty
 from django.shortcuts import get_object_or_404
 from .serializers import ProfessionalSerializer, SpecialtySerializer
+from rest_framework.pagination import PageNumberPagination
 
 class ProfessionalViewSet(viewsets.ModelViewSet):
     queryset = Professional.objects.all()
@@ -74,9 +75,29 @@ class ProfessionalViewSet(viewsets.ModelViewSet):
             professional.remove_specialty(specialty)
             return Response(status=status.HTTP_204_NO_CONTENT)
 
-class SpecialtyViewSet(viewsets.ModelViewSet):
+class CustomPagination(PageNumberPagination):
+    page_size_query_param = 'page_size'
+    max_page_size = 100  # limite máximo opcional
 
+class SpecialtyViewSet(viewsets.ModelViewSet):
     queryset = Specialty.objects.all()
     serializer_class = SpecialtySerializer
     lookup_field = 'uuid'
+    pagination_class = CustomPagination  # Adicione esta linha
+
+    def get_queryset(self):
+        queryset = Specialty.objects.all()
+        search = self.request.query_params.get('search')
+        if search:
+            queryset = queryset.filter(name__icontains=search)
+        return queryset
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
